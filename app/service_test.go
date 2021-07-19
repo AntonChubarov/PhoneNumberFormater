80d2fde16b19d5f7f6838e52d87d19ad7356184c
AntonChubarov <achlp600@gmail.com>
2021-07-19 07:20:15 +0300

    test: more mock tests of service added

--- app/service_test.go
+++ app/service_test.go
@@ -7,11 +7,16 @@ import (
 	"testing"
 )
 
-func TestServiceExecution(t *testing.T)  {
+//rawNumber := "8-050-203-04-50"
+//validNumber := "0502030450"
+//formattedNumber := "+380502030450"
+
+func TestService(t *testing.T)  {
 	ctrl := gomock.NewController(t)
-	//rawNumber := "8-050-203-04-50"
-	//validNumber := "0502030450"
-	//formattedNumber := "+380502030450"
+
+	rawNumber := "8-050-203-04-50"
+	validNumber := "0502030450"
+	formattedNumber := "+380502030450"
 
 	cases := []struct {
 		desc string
@@ -22,12 +27,82 @@ func TestServiceExecution(t *testing.T)  {
 		visualizer domain.Visualizer
 	} {
 		{
-			desc:"Some description",
+			desc:"Function GetAllRawNumbers() of RawStorage should be called once when service Run() executing",
 			rawStorage: func(ctrl *gomock.Controller) *domain.MockRawStorage {
 				mock:=domain.NewMockRawStorage(ctrl)
 				mock.EXPECT().GetAllRawNumbers().Return([]string{}).Times(1)
 				return mock
 			}(ctrl),
+			validStorage: infrastructure.NewValidRAMstorage(),
+			validator: infrastructure.NewRegExValidator(),
+			formatter: infrastructure.NewUkraineFormater(),
+			visualizer: infrastructure.NewConsoleVisualizer(),
+		},
+		{
+			desc:"Function Visualize() of Visualizer should be called twice when service Run() executing",
+			rawStorage: func(ctrl *gomock.Controller) *domain.MockRawStorage {
+				mock:=domain.NewMockRawStorage(ctrl)
+				mock.EXPECT().GetAllRawNumbers().Return([]string{})
+				return mock
+			}(ctrl),
+			validStorage: infrastructure.NewValidRAMstorage(),
+			validator: infrastructure.NewRegExValidator(),
+			formatter: infrastructure.NewUkraineFormater(),
+			visualizer: func(ctrl *gomock.Controller) *domain.MockVisualizer {
+				mock:=domain.NewMockVisualizer(ctrl)
+				mock.EXPECT().Visualize([]string{}).Times(2)
+				return mock
+			}(ctrl),
+		},
+		{
+			desc:"Function Validate() of Validator should be called twice when service Run() executing",
+			rawStorage: func(ctrl *gomock.Controller) *domain.MockRawStorage {
+				mock:=domain.NewMockRawStorage(ctrl)
+				mock.EXPECT().GetAllRawNumbers().Return([]string{""})
+				return mock
+			}(ctrl),
+			validStorage: infrastructure.NewValidRAMstorage(),
+			validator: func(ctrl *gomock.Controller) *domain.MockValidator {
+				mock:=domain.NewMockValidator(ctrl)
+				mock.EXPECT().Validate("").Times(2)
+				return mock
+			}(ctrl),
+			formatter: infrastructure.NewUkraineFormater(),
+			visualizer: infrastructure.NewConsoleVisualizer(),
+		},
+		{
+			desc:"Function TryToFix() and AddCountryCode() of Formatter should be once when GetAllRawNumbers() of RawStorage returns one invalid number",
+			rawStorage: func(ctrl *gomock.Controller) *domain.MockRawStorage {
+				mock:=domain.NewMockRawStorage(ctrl)
+				mock.EXPECT().GetAllRawNumbers().Return([]string{rawNumber})
+				return mock
+			}(ctrl),
+			validStorage: infrastructure.NewValidRAMstorage(),
+			validator: infrastructure.NewRegExValidator(),
+			formatter: func(ctrl *gomock.Controller) *domain.MockFormatter {
+				mock:=domain.NewMockFormatter(ctrl)
+				mock.EXPECT().TryToFix(rawNumber).Return(validNumber).Times(1)
+				mock.EXPECT().AddCountryCode(validNumber).Return(formattedNumber, nil).Times(1)
+				return mock
+			}(ctrl),
+			visualizer: infrastructure.NewConsoleVisualizer(),
+		},
+		{
+			desc:"Function AddValidNumber() and GetAllValidNumbers() of ValidStorage should be called once when GetAllRawNumbers() of RawStorage returns one number",
+			rawStorage: func(ctrl *gomock.Controller) *domain.MockRawStorage {
+				mock:=domain.NewMockRawStorage(ctrl)
+				mock.EXPECT().GetAllRawNumbers().Return([]string{rawNumber})
+				return mock
+			}(ctrl),
+			validStorage: func(ctrl *gomock.Controller) *domain.MockValidStorage {
+				mock:=domain.NewMockValidStorage(ctrl)
+				mock.EXPECT().AddValidNumber(formattedNumber).Times(1)
+				mock.EXPECT().GetAllValidNumbers().Return([]string{formattedNumber}).Times(1)
+				return mock
+			}(ctrl),
+			validator: infrastructure.NewRegExValidator(),
+			formatter: infrastructure.NewUkraineFormater(),
+			visualizer: infrastructure.NewConsoleVisualizer(),
 		},
 	}
 
@@ -35,16 +110,12 @@ func TestServiceExecution(t *testing.T)  {
 		t.Run(c.desc, func(t *testing.T) {
 			service := NewService(
 				c.rawStorage,
-				infrastructure.NewValidRAMstorage(),
-				infrastructure.NewRegExValidator(),
-				infrastructure.NewUkraineFormater(),
-				infrastructure.NewConsoleVisualizer(),
+				c.validStorage,
+				c.validator,
+				c.formatter,
+				c.visualizer,
 				)
-			// act
 			service.Run()
-			// assert
-			//assert.Equal(t, c.wantError, gotError)
-			//assert.Equal(t, c.wantUser, gotUser)
 		})
 	}
 }
